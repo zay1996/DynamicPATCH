cd8756129f08a30f58497403fb2b8fb32d06e214
Zhang <AiZhang@clarku.edu>
2026-08-05 20:55:02 -0400

    update

--- dynamicpatch/create_charts.py
+++ dynamicpatch/create_charts.py
@@ -665,18 +665,18 @@ class Gen_Charts:
                        
         if(option == 'percentage'):
             if(type_ == 'change'):
-                ax.set_ylabel("Annual loss and gain \n (% out of Union Presence)",fontsize = 16)
+                ax.set_ylabel("Annual loss and gain \n (% out of Union Presence)",fontsize = 20)
             if(type_ == 'compare'):
-                ax.set_ylabel("Loss and gain \n (% out of Union Presence)",fontsize = 16)
+                ax.set_ylabel("Loss and gain \n (% out of Union Presence)",fontsize = 20)
         if(option == 'area'):
             if(type_ == 'change' and areaunit == 'pixels'):
-                ax.set_ylabel('Annual loss and gain \n (number of pixels)',fontsize=16)
+                ax.set_ylabel('Annual loss and gain \n (number of pixels)',fontsize=20)
             if(type_ == 'change' and areaunit == 'sqm2'):
-                ax.set_ylabel('Annual loss and gain \n (Square Meters)',fontsize = 16)
+                ax.set_ylabel('Annual loss and gain \n (Square Meters)',fontsize = 20)
             if(type_ == 'change' and areaunit == 'km2'):
-                ax.set_ylabel('Annual loss and gain (km²)',fontsize = 16)
+                ax.set_ylabel('Annual loss and gain (km²)',fontsize = 20)
             if(type_ == 'compare'):
-                ax.set_ylabel("Loss and gain (number of pixels)",fontsize = 16)
+                ax.set_ylabel("Loss and gain (number of pixels)",fontsize = 20)
         #plt.ylim(-2,2)
         ax.axhline(y=0,color='0',linewidth=0.5)
         
